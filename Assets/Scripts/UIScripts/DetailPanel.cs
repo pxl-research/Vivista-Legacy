86a8165389307d3a80ee571a4f89b0e257a92163
Kristof H <kheyndels@gmail.com>
2018-03-22 10:48:11 +0100

    IndexPanel gets disabled when DetailsPanel is open

--- Assets/Scripts/UIScripts/DetailPanel.cs
+++ Assets/Scripts/UIScripts/DetailPanel.cs
@@ -22,6 +22,7 @@ public class DetailPanel : MonoBehaviour
 	public string answerVideoId;
 	
 	private WWW imageDownload;
+	private IndexPanel indexPanel;
 	private float time;
 	private const float refreshTime = 1.0f;
 
@@ -52,8 +53,11 @@ public class DetailPanel : MonoBehaviour
 		}
 	}
 
-	public void Init(VideoSerialize videoToDownload, bool isLocal)
+	public void Init(VideoSerialize videoToDownload, IndexPanel indexPanel, bool isLocal)
 	{
+		this.indexPanel = indexPanel;
+		indexPanel.enabled = false;
+
 		video = videoToDownload;
 
 		videoLength.text = MathHelper.FormatSeconds(video.length);
@@ -84,6 +88,7 @@ public class DetailPanel : MonoBehaviour
 
 	public void Back()
 	{
+		indexPanel.enabled = true;
 		shouldClose = true;
 	}
 
