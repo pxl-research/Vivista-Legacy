ad65c9ac0a993341ccfd5d13b32f4d062fe48719
Kristof H <kheyndels@gmail.com>
2018-03-28 13:11:08 +0200

    Removed unnecessary functions

--- Assets/Scripts/VRDevices.cs
+++ Assets/Scripts/VRDevices.cs
@@ -25,20 +25,6 @@ public static class VRDevices
 	public static bool hasRightController;
 	public static bool hasRemote;
 
-	//NOTE(Kristof): These functions are no londer needed, use LoadedControllerSet
-	//NOTE(Kristof): This functions checks if there are controllers that can be used for Raycasting
-	public static bool HasControllerDevice()
-	{
-		return hasLeftController || hasRightController;
-	}
-
-	//NOTE(Kristof): These functions are no londer needed, use LoadedControllerSet
-	//NOTE(Kristof): This function also checks for the remote
-	public static bool HasAnyDevice()
-	{
-		return hasLeftController || hasRightController || hasRemote;
-	}
-
 	public static void DetectDevices()
 	{
 		var devices = Input.GetJoystickNames();
