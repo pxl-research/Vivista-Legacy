1868df56d8d927f22b333db9625e3fc8010ec384
jitsew <32553997+jitsew@users.noreply.github.com>
2020-10-28 13:23:53 +0100

    Added AsImpL for importing .obj at runtime

--- Assets/Scripts/InteractionPanels/Object3DPanel.cs
+++ Assets/Scripts/InteractionPanels/Object3DPanel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using AsImpL;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,40 +11,86 @@ public class Object3DPanel : MonoBehaviour
 
 	private GameObject objectRenderer;
 
+	[SerializeField]
+	private string filePath = "";
+	[SerializeField]
+	private string objectName = "";
+	[SerializeField]
+	private ImportOptions importOptions = new ImportOptions();
+
+	private ObjectImporter objImporter;
+
 	public void Init(string newTitle, List<string> newPaths)
 	{
 		title.text = newTitle;
 		objectRenderer = GameObject.Find("ObjectRenderer");
-
-		if (newPaths.Count == 1)
+		objImporter = objectRenderer.GetComponent<ObjectImporter>();
+		if (objImporter == null)
 		{
-			//TODO LOAD OBJ
-		} else
-		{
-			//TODO LOAD OBJ WITH MTL
+			objImporter = objectRenderer.AddComponent<ObjectImporter>();
 		}
-		object3d.transform.parent = objectRenderer.transform;
 
-		var renderer = object3d.GetComponentInChildren<Renderer>();
-		var bound = renderer.bounds;
-		var center = bound.center;
-		var radius = bound.extents.magnitude;
-		Debug.Log(bound.ToString() + "\t" + center.ToString() + "\t" + radius.ToString());
+		objImporter.ImportingComplete += SetObjectProperties;
 
-		object3d.transform.localScale = new Vector3(50, 50, 50);
-		object3d.transform.localRotation = new Quaternion(0, 0, 0, 0);
-		object3d.transform.localPosition = new Vector3(0, 0, -50);
-		object3d.SetLayer(12);
-		object3d.SetActive(false);
+		if (newPaths.Count >= 1)
+		{
+			filePath = newPaths[0];
+			objectName = filePath.Substring(filePath.LastIndexOf("\\") + 1);
+			objectName = objectName.Substring(0, objectName.IndexOf("."));
+			objImporter.ImportModelAsync(objectName, filePath, objectRenderer.transform, importOptions);
+		} 
+	}
+
+	private void SetObjectProperties()
+	{
+		var objects3d = objectRenderer.GetComponentsInChildren<Transform>(true);
+		for (int i = 0; i < objects3d.Length; i++)
+		{
+			var tempObject = objects3d[i];
+			if (tempObject.name == objectName)
+			{
+				object3d = tempObject.gameObject;
+				var children = object3d.GetComponentsInChildren<Transform>();
+				for (int j = 0; j < children.Length; j++)
+				{
+					children[j].localPosition = Vector3.zero;
+				}
+				var rotation = object3d.transform.localRotation.eulerAngles;
+				rotation.x = -90;
+				object3d.transform.localRotation = Quaternion.Euler(rotation);
+				object3d.transform.localPosition = new Vector3(0, 0, -50);
+				object3d.transform.localScale = new Vector3(10, 10, 10);
+				object3d.SetLayer(12);
+				object3d.SetActive(false);
+				
+				break;
+			}
+		}
+	}
+
+	private void Update()
+	{
+		if (object3d != null)
+		{
+			var oldRotation = object3d.transform.localRotation.eulerAngles;
+			oldRotation.y += 0.1f;
+			object3d.transform.localRotation = Quaternion.Euler(oldRotation);
+		}
 	}
 
 	private void OnEnable()
 	{
-		object3d.SetActive(true);
+		if (object3d != null)
+		{
+			object3d.SetActive(true);
+		}
 	}
 
 	private void OnDisable()
 	{
-		object3d.SetActive(false);
+		if (object3d != null)
+		{
+			object3d.SetActive(false);
+		}
 	}
 }
