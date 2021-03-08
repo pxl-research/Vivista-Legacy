69b1dd90401d544db084d8e14cc3100fa300bfff
Saticmotion <simon_verbeke_@hotmail.com>
2021-03-08 16:56:51 +0100

    Input bindings potentially fixed

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -17,38 +17,6 @@ namespace Valve.VR
     public class SteamVR_Input_ActionSet_default : Valve.VR.SteamVR_ActionSet
     {
         
-        public virtual SteamVR_Action_Boolean InteractUI
-        {
-            get
-            {
-                return SteamVR_Actions.default_InteractUI;
-            }
-        }
-        
-        public virtual SteamVR_Action_Boolean Teleport
-        {
-            get
-            {
-                return SteamVR_Actions.default_Teleport;
-            }
-        }
-        
-        public virtual SteamVR_Action_Boolean GrabPinch
-        {
-            get
-            {
-                return SteamVR_Actions.default_GrabPinch;
-            }
-        }
-        
-        public virtual SteamVR_Action_Boolean GrabGrip
-        {
-            get
-            {
-                return SteamVR_Actions.default_GrabGrip;
-            }
-        }
-        
         public virtual SteamVR_Action_Pose Pose
         {
             get
@@ -70,14 +38,6 @@ namespace Valve.VR
             get
             {
                 return SteamVR_Actions.default_SkeletonRightHand;
-            }
-        }
-        
-        public virtual SteamVR_Action_Single Squeeze
-        {
-            get
-            {
-                return SteamVR_Actions.default_Squeeze;
             }
         }
         
