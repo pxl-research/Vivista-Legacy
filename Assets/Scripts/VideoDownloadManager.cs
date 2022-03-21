33807f650c88ae9b7582c4c6d63873c28a3c773b
Saticmotion <simon_verbeke_@hotmail.com>
2022-03-21 01:00:10 +0100

    temp

--- Assets/Scripts/VideoDownloadManager.cs
+++ Assets/Scripts/VideoDownloadManager.cs
@@ -204,7 +204,8 @@ public class VideoDownloadManager : MonoBehaviour
 			go.transform.SetParent(Canvass.main.transform, false);
 			var panel = go.GetComponent<DownloadInProgressPanel>();
 			Canvass.modalBackground.SetActive(true);
-
+			dsadsa
+				//TODO(Simon): Fix Abort Screen
 			panel.OnAbortDownload += () =>
 			{
 				client.CancelPendingRequests();
